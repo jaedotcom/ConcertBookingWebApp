af3b106466d762c71e689e9b07406e2902368095
Arnav <arnard76@gmail.com>
2024-04-03 21:25:02 +1300

    setting up concert service 🆒😎 && creating method to get concert summaries

--- concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
@@ -1,7 +1,40 @@
 package proj.concert.service.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import proj.concert.common.dto.ConcertSummaryDTO;
+import proj.concert.service.domain.Concert;
+
+@Path("/concert-service")
 public class ConcertResource {
 
-    // TODO Implement this.
+    @GET
+    @Path("/concerts/summaries")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveConcertSummaries() {
+        EntityManager em = PersistenceManager.instance().createEntityManager();
+        List<ConcertSummaryDTO> summaries = new ArrayList<ConcertSummaryDTO>();
+
+        try {
+            TypedQuery<Concert> query = em.createQuery("select c from Concert c", Concert.class);
+            for (Concert concert : query.getResultList()) {
+                summaries.add(new ConcertSummaryDTO(concert.getId(), concert.getTitle(), concert.getImageName()));
+            }
+
+        } finally {
+            em.close();
+        }
+
+        return Response.ok(summaries).build();
+    }
 
 }
