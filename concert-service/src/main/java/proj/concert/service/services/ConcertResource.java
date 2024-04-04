83bbbc1b3f06287b1c6c6f88fde4ee81293beec0
Arnav <arnard76@gmail.com>
2024-04-04 23:06:23 +1300

    ENDPOINT `GET concerts/{id}` 😄😄😄
    hopefully this doesn't die 😇

--- concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
@@ -7,11 +7,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import proj.concert.service.mapper.ConcertMapper;
 import proj.concert.common.dto.ConcertSummaryDTO;
+import proj.concert.common.dto.ConcertDTO;
 import proj.concert.service.domain.Concert;
 
 @Path("/concert-service")
@@ -37,4 +40,24 @@ public class ConcertResource {
         return Response.ok(summaries).build();
     }
 
+    @GET
+    @Path("/concerts/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveConcert(@PathParam(value = "id") long id) {
+        EntityManager em = PersistenceManager.instance().createEntityManager();
+        ConcertDTO concertDto = null;
+
+        try {
+            Concert concert = em.find(Concert.class, id);
+
+            concertDto = ConcertMapper.toDto(concert);
+        } catch (Exception e) {
+            return Response.status(404).build();
+        } finally {
+            em.close();
+        }
+
+        return Response.ok(concertDto).build();
+
+    }
 }
