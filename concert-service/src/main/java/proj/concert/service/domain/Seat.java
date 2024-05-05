b9fcae089fcba3d81f01c00d1baaf09373585109
jaedotcom <k.jaeha@gmail.com>
2024-05-05 13:53:41 +1200

    Added JPA dependencies to fix @Embeddable annotation issue in SeatKey class

--- concert-service/src/main/java/proj/concert/service/domain/Seat.java
+++ concert-service/src/main/java/proj/concert/service/domain/Seat.java
@@ -1,116 +1,103 @@
-package proj.concert.service.domain;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.Version;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import javax.persistence.CascadeType;
+import javax.persistence.EmbeddedId;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Version;
-import javax.persistence.CascadeType;
-
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
-/**
- * Domain model class to represent seats at the concert venue.
- * <p>
- * A Seat describes a seat in terms of:
- * label the seat label
- * price the price
- * date the concert date
- * isBooked - whether it is booked or not
- */
 @Entity
 public class Seat implements Serializable {
-	@Id
-	private String label;
-	@Id
-	private LocalDateTime date;
-	private BigDecimal price;
-	private boolean isBooked = false;
-	@Version
-	private int version;
+    @EmbeddedId
+    private SeatKey id;
+    private BigDecimal price;
+    private boolean isBooked = false;
+    @Version
+    private int version;
 
-	@ManyToMany(mappedBy = "seats", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private Set<Booking> bookings;
+    @ManyToMany(mappedBy = "seats", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Booking> bookings;
 
-	public Seat() {
-	}
+    public Seat() {
+    }
 
-	public Seat(String label, LocalDateTime date, BigDecimal price) {
-		this.label = label;
-		this.price = price;
-		this.date = date;
-	}
+    public Seat(String label, LocalDateTime date, BigDecimal price) {
+        this.id = new SeatKey(label, date);
+        this.price = price;
+    }
 
-	public String getLabel() {
-		return label;
-	}
+    public String getLabel() {
+        return id.getLabel();
+    }
 
-	public void setLabel(String label) {
-		this.label = label;
-	}
+    public LocalDateTime getDate() {
+        return id.getDate();
+    }
 
-	public BigDecimal getPrice() {
-		return price;
-	}
+    public BigDecimal getPrice() {
+        return price;
+    }
 
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 
-	public LocalDateTime getDate(){
-		return date;
-	}
+    public boolean isBooked() {
+        return isBooked;
+    }
 
-	public void setDate(LocalDateTime date){
-		this.date = date;
-	}
+    public void setIsBooked(boolean isBooked) {
+        this.isBooked = isBooked;
+    }
 
-	public boolean getIsBooked() {
-		return this.isBooked;
-	}
+    public int getVersion(){
+        return this.version;
+    }
 
-	public void setIsBooked(boolean isBooked) {
-		this.isBooked = isBooked;
-	}
+    public void setVersion(int newVersion){
+        this.version = newVersion;
+    }
 
-	public int getVersion(){
-		return this.version;
-	}
+    public Set<Booking> getBookings() {
+        return bookings;
+    }
 
-	public void setVersion(int newVersion){
-		this.version = newVersion;
-	}
+    public void setBookings(Set<Booking> bookings) {
+        this.bookings = bookings;
+    }
 
-	@Override
-	public String toString() {
-		return label;
-	}
+    @Override
+    public String toString() {
+        return id.getLabel();
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
 
-		if (o == null || getClass() != o.getClass())
-			return false;
+        if (o == null || getClass() != o.getClass())
+            return false;
 
-		Seat seat = (Seat) o;
+        Seat seat = (Seat) o;
 
-		return new EqualsBuilder()
-				.append(label, seat.label)
-				.isEquals();
-	}
+        return new EqualsBuilder()
+                .append(id, seat.id)
+                .isEquals();
+    }
 
-	@Override
-	public int hashCode() {
-		return new HashCodeBuilder(17, 37)
-				.append(label)
-				.toHashCode();
-	}
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .toHashCode();
+    }
 }
